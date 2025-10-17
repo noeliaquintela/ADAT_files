218ddebb269d483605a02a6d8fc435b6d29f7d95
profeCarballeira <carballeira2024@gmx.es>
2025-10-17 09:33:05 +0200

    Guardar CSV

--- 05-csv/Form1.Designer.cs
+++ 05-csv/Form1.Designer.cs
@@ -28,20 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btnLeerCSV = new Button();
+            btnLeerCSVpuntocoma = new Button();
             label1 = new Label();
             btnLeerCSVHelper = new Button();
+            btnLeerCSVhelperpuntocoma = new Button();
+            btnLeerCSVHelperSinEnc = new Button();
+            btnGuardarCSVHelper = new Button();
             SuspendLayout();
             // 
-            // btnLeerCSV
+            // btnLeerCSVpuntocoma
             // 
-            btnLeerCSV.Location = new Point(12, 24);
-            btnLeerCSV.Name = "btnLeerCSV";
-            btnLeerCSV.Size = new Size(180, 52);
-            btnLeerCSV.TabIndex = 0;
-            btnLeerCSV.Text = "Leer CSV";
-            btnLeerCSV.UseVisualStyleBackColor = true;
-            btnLeerCSV.Click += btnLeerCSV_Click;
+            btnLeerCSVpuntocoma.Location = new Point(12, 24);
+            btnLeerCSVpuntocoma.Name = "btnLeerCSVpuntocoma";
+            btnLeerCSVpuntocoma.Size = new Size(180, 52);
+            btnLeerCSVpuntocoma.TabIndex = 0;
+            btnLeerCSVpuntocoma.Text = "Leer CSV PUNTOYCOMA";
+            btnLeerCSVpuntocoma.UseVisualStyleBackColor = true;
+            btnLeerCSVpuntocoma.Click += btnLeerCSV_Click;
             // 
             // label1
             // 
@@ -62,14 +65,47 @@
             btnLeerCSVHelper.UseVisualStyleBackColor = true;
             btnLeerCSVHelper.Click += btnLeerCSVHelper_Click;
             // 
+            // btnLeerCSVhelperpuntocoma
+            // 
+            btnLeerCSVhelperpuntocoma.Location = new Point(12, 168);
+            btnLeerCSVhelperpuntocoma.Name = "btnLeerCSVhelperpuntocoma";
+            btnLeerCSVhelperpuntocoma.Size = new Size(180, 52);
+            btnLeerCSVhelperpuntocoma.TabIndex = 3;
+            btnLeerCSVhelperpuntocoma.Text = "Leer CSV Helper (punotcoma)";
+            btnLeerCSVhelperpuntocoma.UseVisualStyleBackColor = true;
+            btnLeerCSVhelperpuntocoma.Click += button1_Click;
+            // 
+            // btnLeerCSVHelperSinEnc
+            // 
+            btnLeerCSVHelperSinEnc.Location = new Point(23, 247);
+            btnLeerCSVHelperSinEnc.Name = "btnLeerCSVHelperSinEnc";
+            btnLeerCSVHelperSinEnc.Size = new Size(180, 52);
+            btnLeerCSVHelperSinEnc.TabIndex = 4;
+            btnLeerCSVHelperSinEnc.Text = "Leer CSV Helper sin encabezado";
+            btnLeerCSVHelperSinEnc.UseVisualStyleBackColor = true;
+            btnLeerCSVHelperSinEnc.Click += button2_Click;
+            // 
+            // btnGuardarCSVHelper
+            // 
+            btnGuardarCSVHelper.Location = new Point(23, 329);
+            btnGuardarCSVHelper.Name = "btnGuardarCSVHelper";
+            btnGuardarCSVHelper.Size = new Size(180, 52);
+            btnGuardarCSVHelper.TabIndex = 5;
+            btnGuardarCSVHelper.Text = "Guardar CSV Helper";
+            btnGuardarCSVHelper.UseVisualStyleBackColor = true;
+            btnGuardarCSVHelper.Click += btnGuardarCSVHelper_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btnGuardarCSVHelper);
+            Controls.Add(btnLeerCSVHelperSinEnc);
+            Controls.Add(btnLeerCSVhelperpuntocoma);
             Controls.Add(btnLeerCSVHelper);
             Controls.Add(label1);
-            Controls.Add(btnLeerCSV);
+            Controls.Add(btnLeerCSVpuntocoma);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -78,8 +114,11 @@
 
         #endregion
 
-        private Button btnLeerCSV;
+        private Button btnLeerCSVpuntocoma;
         private Label label1;
         private Button btnLeerCSVHelper;
+        private Button btnLeerCSVhelperpuntocoma;
+        private Button btnLeerCSVHelperSinEnc;
+        private Button btnGuardarCSVHelper;
     }
 }
--- 05-csv/Form1.cs
+++ 05-csv/Form1.cs
@@ -41,9 +41,9 @@ namespace _05_csv
         {
             string fichero = "personas.csv";
 
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture) 
-            { 
-            
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+
             };
 
             StreamReader sr = new StreamReader(fichero);
@@ -57,6 +57,88 @@ namespace _05_csv
                                         persona.edad + " " +
                                         persona.altura + System.Environment.NewLine;
                 }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fichero = "personas_puntocoma.csv";
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";" //establece el delimitador en ";"               
+            };
+
+            using StreamReader sr = new StreamReader(fichero);
+            using (var csv = new CsvReader(sr, config))
+            {
+                var personas = csv.GetRecords<Persona>();
+
+                foreach (var persona in personas)
+                {
+                    this.label1.Text += persona.nombre + " " +
+                                        persona.edad + " " +
+                                        persona.altura + System.Environment.NewLine;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string fichero = "personas_puntocoma_sinencabezados.csv";
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";", //establece el delimitador en ";"               
+                HasHeaderRecord = false //csv sin encabezados
+            };
+
+            StreamReader sr = new StreamReader(fichero);
+            using (var csv = new CsvReader(sr, config))
+            {
+                var personas = csv.GetRecords<Persona>();
+
+                foreach (var persona in personas)
+                {
+                    this.label1.Text += persona.nombre + " " +
+                                        persona.edad + " " +
+                                        persona.altura + System.Environment.NewLine;
+                }
+            }
+        }
+
+        private void btnGuardarCSVHelper_Click(object sender, EventArgs e)
+        {
+            string fichero = "personas_guardadas.csv";
+
+            // Crear una lista de ejemplo de personas (puedes reemplazarla con datos dinámicos)
+            var personas = new List<Persona>
+            {
+                new Persona { nombre = "Carlos", edad = 35, altura = 170 },
+                new Persona { nombre = "María", edad = 28, altura = 160 },
+                new Persona { nombre = "Pedro", edad = 40, altura = 180 }
+            };
+
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                Delimiter = ";", // Establece el delimitador en ";"
+                HasHeaderRecord = true // Incluye encabezados en el CSV
+            };
+
+            try
+            {
+                using (var sw = new StreamWriter(fichero, false, new System.Text.UTF8Encoding(true)))
+                using (var csv = new CsvWriter(sw, config))
+                {
+                    csv.WriteRecords(personas);
+                }
+
+                MessageBox.Show("Lista de personas guardada correctamente en el fichero CSV.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar el fichero CSV: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
